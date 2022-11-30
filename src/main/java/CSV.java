b26719ef432a5240709317d9c7804e05d3119b9f
rcbarriga <rubencostab97@gmail.com>
2022-11-30 16:20:19 +0100

    csv

--- src/main/java/CSV.java
+++ src/main/java/CSV.java
@@ -3,7 +3,7 @@ import java.io.FileReader;
 import com.opencsv.*;
 public class CSV {
 
-    public void readCSV {
+    public void readCSV (){
         try {
             CSVReader lector = new CSVReader(new FileReader("hola.csv"));
         } catch (FileNotFoundException e) {
